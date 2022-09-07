fa10fd0f6a70a75e9581b39b46ea45dc75c08565
mitchcoded <husslet1@yahoo.com>
2022-09-07 09:25:20 +0100

    first commit

---
+++ home.js
@@ -0,0 +1,11 @@
+
+let contactEl = document.getElementById("contact-btn") 
+let contactEle = document.getElementById("contact-btn2")
+
+contactEl.addEventListener("click", function(){
+    window.location.href= "contact.html";
+})
+
+contactEle.addEventListener("click", function(){
+    window.location.href= "contact.html";
+})
